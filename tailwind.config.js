b3f39cba571edfed986ea51819738b140bcf1aba
salby <salby966@gmail.com>
2021-09-07 16:49:30 +0700

    progress menambah background image, resize slider, menambah content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screen: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+
+    maxHeight: {
+      '0' : '0',
+      '1/4' : '25%',
+      '1/2' : '50%',
+      '3/4' : '75%',
+      'full' : '100%',
+
+    },
+
+    padding: {
+      'sm': '8px',
+      'md': '16px',
+      'lg': '24px',
+      'xl': '48px',
+      'next': '400px',
+    },
+
+    extend: {
+      backgroundImage: theme => ({
+        'green-pattern' : "url(/public/img/bg-green.png)",
+      })
+    },
   },
   variants: {
     extend: {},
